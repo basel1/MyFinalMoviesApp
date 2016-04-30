df951376878f39b81b248309a93082bc6ccc92df
Basel Salah <basel.dragon13@gmail.com>
2016-04-30 16:25:20 +0200

    commenting the exception

--- app/src/main/java/com/movies/basel/mymoviesapp/MainFragment.java
+++ app/src/main/java/com/movies/basel/mymoviesapp/MainFragment.java
@@ -128,12 +128,12 @@ public void onActivityCreated(Bundle savedInstanceState) {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
+      /*  if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
-        }
+        }*/
     }
 
     @Override
